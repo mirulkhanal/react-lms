c6554e6f276083a4ad768efe0bc85be39e3eedab
Mirul Khanal <mirulkhanal@gmail.com>
2021-03-21 02:32:02 +0545

    added user type check for admin functionalities

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,9 +4,15 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (token) {
     const user = jwt.verify(token.split(' ')[1], 'secretcode')
-    req.user = user
-    console.log(user)
-    next()
+    if (user.type !== 'admin') {
+      return res.status(401).send({
+        error: 'Admin access only',
+      })
+    } else {
+      req.user = user
+      console.log(user)
+      next()
+    }
   } else {
     res.status(401).send({
       error: 'Unauthorized',
